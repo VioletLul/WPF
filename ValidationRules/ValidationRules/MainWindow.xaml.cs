5ff956180c17b0318341487d81c23be0c5973e5a
Jason Roesser <j.roesser-ecdl@t-online.de>
2023-11-14 09:00:30 +0100

    Added ValidationRules and ValidationsOnExceptions

---
+++ ValidationRules/ValidationRules/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace ValidationRules
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Message { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
---
+++ ValidationsOnExceptions/ValidationsOnExceptions/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace ValidationsOnExceptions
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _message;
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                if (value == string.Empty)
+                    throw new ArgumentException();
+                else
+                    _message = value;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
